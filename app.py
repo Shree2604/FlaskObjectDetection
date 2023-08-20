6af956f76127aab47b33a085dc9bb5b9b42c2316
M SHREERAJ <125468219+Shree2604@users.noreply.github.com>
2023-08-20 12:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-
-from flask_bootstrap import Bootstrap
-
-from werkzeug.utils import secure_filename
-
 import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
 import tensorflow as tf
-from collections import defaultdict
-from io import StringIO
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 from PIL import Image
-sys.path.append("..")
+
+# Import label_map_util and visualization_utils from the utils directory
 from utils import label_map_util
 from utils import visualization_utils as vis_util
+
+# Rest of your imports remain the same
+
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
@@ -23,21 +19,15 @@ NUM_CLASSES = 90
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+    od_graph_def = tf.compat.v1.GraphDef()  # Use tf.compat.v1 for compatibility
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -46,14 +36,11 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -61,18 +48,16 @@ def upload():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',
-                                filename=filename))
-
+        return redirect(url_for('uploaded_file', filename=filename))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     PATH_TO_TEST_IMAGES_DIR = app.config['UPLOAD_FOLDER']
-    TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR,filename.format(i)) for i in range(1, 2) ]
+    TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, filename.format(i)) for i in range(1, 2)]
     IMAGE_SIZE = (12, 8)
 
     with detection_graph.as_default():
-        with tf.Session(graph=detection_graph) as sess:
+        with tf.compat.v1.Session(graph=detection_graph) as sess:
             for image_path in TEST_IMAGE_PATHS:
                 image = Image.open(image_path)
                 image_np = load_image_into_numpy_array(image)
@@ -94,10 +79,9 @@ def uploaded_file(filename):
                     use_normalized_coordinates=True,
                     line_thickness=8)
                 im = Image.fromarray(image_np)
-                im.save('uploads/'+filename)
+                im.save('uploads/' + filename)
 
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
-
+    app.run(debug=True, host='0.0.0.0', port=5000)
